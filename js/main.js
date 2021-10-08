11336b41b8356b4d4fec15209f8de78853385850
Akash Prasher <prince.akash.prasher@gmail.com>
2021-10-08 20:24:14 +0530

    updating files for section-hero
    Signed-off-by: Akash Prasher <prince.akash.prasher@gmail.com>

--- js/main.js
+++ js/main.js
@@ -5,11 +5,8 @@ window.addEventListener("scroll", () => {
   if (window.pageYOffset >= 100) {
     naviagtionMenu.classList.add("menu");
     naviagtionMenu.classList.remove("menu-mid");
-    console.log("showing" + window.pageYOffset);
   } else {
     naviagtionMenu.classList.remove("menu");
-    // naviagtionMenu.classList.remove("menu-mid");
-    console.log("showing else" + window.pageYOffset);
   }
 
   if (80 < window.pageYOffset && window.pageYOffset < 99) {
